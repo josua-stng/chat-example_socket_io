082d4dc34de871e2307bed5e3cee8fcdbe2f7203
josua-stng <josuasitanggang51@gmail.com>
2022-05-03 12:06:40 +0700

    add file

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
